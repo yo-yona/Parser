6981f19c74299daf12a25cb58d4ff0e8ce002b62
svetlanayo <svetlanya05@mail.ru>
2021-09-30 20:46:18 +0300

    Project created

---
+++ SimbirSoftParser/SimbirSoftParser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimbirSoftParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
