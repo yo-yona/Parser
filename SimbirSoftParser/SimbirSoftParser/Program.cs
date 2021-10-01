00061472b08515033ffb74da7a13a2909484cd5a
svetlanayo <svetlanya05@mail.ru>
2021-10-01 20:43:38 +0300

    Added error detection

--- SimbirSoftParser/SimbirSoftParser/Program.cs
+++ SimbirSoftParser/SimbirSoftParser/Program.cs
@@ -9,33 +9,43 @@ namespace SimbirSoftParser
 {
     class Program
     {
+        //(HttpWebRequest)
         //https://docs.microsoft.com/en-us/dotnet/framework/network-programming/how-to-request-data-using-the-webrequest-class
         static void Main(string[] args)
         {
-            // Create a request for the URL.
-            WebRequest request = WebRequest.Create("https://https://www.simbirsoft.com/");
-            // If required by the server, set the credentials.
-            request.Credentials = CredentialCache.DefaultCredentials;
-
-            // Get the response.
-            WebResponse response = request.GetResponse();
-            // Display the status.
-            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-
-            // Get the stream containing content returned by the server.
-            // The using block ensures the stream is automatically closed.
-            using (Stream dataStream = response.GetResponseStream())
+            try
             {
-                // Open the stream using a StreamReader for easy access.
-                StreamReader reader = new StreamReader(dataStream);
-                // Read the content.
-                string responseFromServer = reader.ReadToEnd();
-                // Display the content.
-                Console.WriteLine(responseFromServer);
-            }
+                // Create a request for the URL.
+                var request = WebRequest.Create("https://www.simbirsoft.com/");
 
-            // Close the response.
-            response.Close();
+                // Get the response.
+                WebResponse response = request.GetResponse();
+                // Display the status.
+                Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+                // Get the stream containing content returned by the server.
+                // The using block ensures the stream is automatically closed.
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    // Open the stream using a StreamReader for easy access.
+                    StreamReader reader = new StreamReader(dataStream);
+                    // Read the content.
+                    string responseFromServer = reader.ReadToEnd();
+                    // Display the content.
+                    Console.WriteLine(responseFromServer);
+                }
+
+                // Close the response.
+                response.Close();
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine("\r\nWebException Raised. The following error occurred : {0}", e.Status);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nThe following Exception was raised : {0}", e.Message);
+            }
         }
     }
 }
