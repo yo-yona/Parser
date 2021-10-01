635a81ab1ee6284f1023d680199f7582d5c51099
svetlanayo <svetlanya05@mail.ru>
2021-10-01 20:26:13 +0300

    Made reading of the page available

--- SimbirSoftParser/SimbirSoftParser/Program.cs
+++ SimbirSoftParser/SimbirSoftParser/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Net;
+using HtmlAgilityPack;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace SimbirSoftParser
 {
     class Program
     {
+        //https://docs.microsoft.com/en-us/dotnet/framework/network-programming/how-to-request-data-using-the-webrequest-class
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create a request for the URL.
+            WebRequest request = WebRequest.Create("https://https://www.simbirsoft.com/");
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+            // Get the stream containing content returned by the server.
+            // The using block ensures the stream is automatically closed.
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+                // Display the content.
+                Console.WriteLine(responseFromServer);
+            }
+
+            // Close the response.
+            response.Close();
         }
     }
 }
