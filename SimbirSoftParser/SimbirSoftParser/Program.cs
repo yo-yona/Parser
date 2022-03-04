94028dbb1d0cca33081c80ec46b86306ceef4921
svetlanayo <svetlanya05@mail.ru>
2022-03-04 23:29:04 +0300

    Ability to access any site

--- SimbirSoftParser/SimbirSoftParser/Program.cs
+++ SimbirSoftParser/SimbirSoftParser/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace SimbirSoftParser
+﻿using System;
+
+namespace SimbirSoftParser
 {
     class Program
     {
         static void Main(string[] args)
         {
-            WebAgent webConnection = new WebAgent("https://simbirsoft.com/");
+            Console.WriteLine("Введите сайт для проверки");
+            string site = Console.ReadLine();
+            if (String.IsNullOrWhiteSpace(site))
+                site = "https://simbirsoft.com/";
+            WebAgent webConnection = new WebAgent(site);
             webConnection.CountStatistics();
             webConnection.PrintStatistics();
         }
